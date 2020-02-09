233b94417f0c3a3f36c2ec3f433e74279a9b394b
allennaliath <allennaliath@gmail.com>
2020-02-09 09:37:00 -0500

    created a new activity that displays the stock data

---
+++ app/src/main/java/com/example/financeforteens/StockDataDisplay.java
@@ -0,0 +1,14 @@
+package com.example.financeforteens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StockDataDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock_data_display);
+    }
+}
