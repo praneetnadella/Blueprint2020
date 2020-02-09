0c62e123bf5faa26e65fcbe2b8683d4740fdeda1
Neil Kachappilly <neilkach@gmail.com>
2020-02-09 14:00:00 -0500

    storage

--- app/src/main/java/com/example/financeforteens/MainActivity.java
+++ app/src/main/java/com/example/financeforteens/MainActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobile.client.Callback;
 import com.amazonaws.mobile.client.UserStateDetails;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +35,23 @@ public class MainActivity extends AppCompatActivity {
         );
 
  */
+        AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
+            @Override
+            public void onResult(UserStateDetails userStateDetails) {
+                try {
+                    Amplify.addPlugin(new AWSS3StoragePlugin());
+                    Amplify.configure(getApplicationContext());
+                    Log.i("StorageQuickstart", "All set and ready to go!");
+                } catch (Exception e) {
+                    Log.e("StorageQuickstart", e.getMessage());
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.e("StorageQuickstart", "Initialization error.", e);
+            }
+        });
     }
 
     public void login (View v) {
