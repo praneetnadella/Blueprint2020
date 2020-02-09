8b104705e760574b66246ba122a388b5b29c1f1d
Akshay Shinde <Akshay@Akshays-MacBook-Air.local>
2020-02-09 12:50:28 -0500

    changes

--- app/src/main/java/com/example/financeforteens/FinanceActivity.java
+++ app/src/main/java/com/example/financeforteens/FinanceActivity.java
@@ -4,6 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FinanceActivity extends AppCompatActivity {
 
@@ -11,6 +19,39 @@ public class FinanceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finance);
+        this.simpleAdapterView();
+    }
+
+    private void simpleAdapterView(){
+        setTitle("Items");
+
+        String[] titleArr = { "Name", "Sex", "Age", "Location","Email"};
+        String[] descArr = { "Jerry", "Male", "43", "Singapore", "webmaster@dev2qa.com" };
+
+        ArrayList<Map<String,Object>> itemDataList = new ArrayList<Map<String,Object>>();;
+
+        int titleLen = titleArr.length;
+        for(int i =0; i < titleLen; i++) {
+            Map<String,Object> listItemMap = new HashMap<String,Object>();
+            listItemMap.put("title", titleArr[i]);
+            listItemMap.put("description", descArr[i]);
+            itemDataList.add(listItemMap);
+        }
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this,itemDataList,android.R.layout.simple_list_item_2,
+                new String[]{"title","description"},new int[]{android.R.id.text1,android.R.id.text2});
+
+        ListView listView = findViewById(R.id.ListViewExample);
+        listView.setAdapter(simpleAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int index, long l) {
+                Object clickItemObj = adapterView.getAdapter().getItem(index);
+                Toast.makeText(FinanceActivity.this, "You clicked " + clickItemObj.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 }
