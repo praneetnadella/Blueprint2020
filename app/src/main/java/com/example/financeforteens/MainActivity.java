81f791bb706cac2970d02de60b9c991b1a960a27
Neil Kachappilly <neilkach@gmail.com>
2020-02-09 11:01:32 -0500

    testing changes

--- app/src/main/java/com/example/financeforteens/MainActivity.java
+++ app/src/main/java/com/example/financeforteens/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.financeforteens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login (View v) {
+        Intent loginIntent = new Intent (this, LoginActivity.class);
+        startActivity(loginIntent);
+    }
+
+    public void addCost(View v) {
+        Intent loginIntent = new Intent (this, AddCostActivity.class);
+        startActivity(loginIntent);
+    }
+
+    public void finance(View v) {
+        Intent loginIntent = new Intent (this, FinanceActivity.class);
+        startActivity(loginIntent);
+    }
+
+    public void stockDataEntry(View v) {
+        Intent loginIntent = new Intent (this, StockDataEntry.class);
+        startActivity(loginIntent);
     }
 }
