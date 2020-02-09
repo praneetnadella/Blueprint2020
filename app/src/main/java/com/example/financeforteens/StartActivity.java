391df77803e6517b3614a29226542b79c971de18
Neil Kachappilly <neilkach@gmail.com>
2020-02-09 15:43:05 -0500

    hel

--- app/src/main/java/com/example/financeforteens/StartActivity.java
+++ app/src/main/java/com/example/financeforteens/StartActivity.java
@@ -8,7 +8,7 @@ import android.os.Handler;
 
 public class StartActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
