bd303882457c430f3ae1cb52c0ffbe49962ab9bf
Akshay Shinde <Akshay@Akshays-MacBook-Air.local>
2020-02-09 12:54:38 -0500

    changes

--- app/src/main/java/com/example/financeforteens/LoginActivity.java
+++ app/src/main/java/com/example/financeforteens/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.financeforteens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -10,6 +12,11 @@ import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobile.client.Callback;
 import com.amazonaws.mobile.client.SignInUIOptions;
 import com.amazonaws.mobile.client.UserStateDetails;
+import com.amazonaws.mobile.client.results.SignUpResult;
+import com.amazonaws.mobile.client.results.UserCodeDeliveryDetails;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -22,39 +29,53 @@ public class LoginActivity extends AppCompatActivity {
 
         AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
 
-                    @Override
-                    public void onResult(UserStateDetails userStateDetails) {
-                        Log.i("INIT", "onResult: " + userStateDetails.getUserState());
-                    }
-
-                    @Override
-                    public void onError(Exception e) {
-                        Log.e("INIT", "Initialization error.", e);
-                    }
+            @Override
+            public void onResult(UserStateDetails userStateDetails) {
+                Log.i(TAG, userStateDetails.getUserState().toString());
+                switch (userStateDetails.getUserState()){
+                    case SIGNED_IN:
+                        Intent i = new Intent(LoginActivity.this, MainScreenActivity.class);
+                        startActivity(i);
+                        break;
+                    case SIGNED_OUT:
+                        showSignIn();
+                        break;
+                    default:
+                        AWSMobileClient.getInstance().signOut();
+                        showSignIn();
+                        break;
                 }
-        );
+            }
 
+            @Override
+            public void onError(Exception e) {
+                Log.e(TAG, e.toString());
+            }
+        });
+    }
+
+    private void showSignIn() {
+        try {
+            AWSMobileClient.getInstance().showSignIn(this,
+                    SignInUIOptions.builder().nextActivity(MainScreenActivity.class).build());
+        } catch (Exception e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+/*
         AWSMobileClient.getInstance().showSignIn(
                 this,
                 SignInUIOptions.builder()
-                        .nextActivity(MainScreenActivity.class)
+                        .nextActivity(MainActivity.class)
+                        .backgroundColor(R.color.colorPrimaryDark)
+                        .canCancel(false)
                         .build(),
                 new Callback<UserStateDetails>() {
                     @Override
                     public void onResult(UserStateDetails result) {
                         Log.d(TAG, "onResult: " + result.getUserState());
-                        switch (result.getUserState()){
-                            case SIGNED_IN:
-                                Log.i("INIT", "logged in!");
-                                break;
-                            case SIGNED_OUT:
-                                Log.i(TAG, "onResult: User did not choose to sign-in");
-                                break;
-                            default:
-                                AWSMobileClient.getInstance().signOut();
-                                break;
-                        }
                     }
+
 
                     @Override
                     public void onError(Exception e) {
@@ -62,6 +83,7 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 }
         );
+
 
         AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
             @Override
@@ -96,5 +118,6 @@ public class LoginActivity extends AppCompatActivity {
                 Log.e("INIT", e.toString());
             }
         });
-    }
+
+         */
 }
